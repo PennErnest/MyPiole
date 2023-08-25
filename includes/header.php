df1feed95269477ea340778fc53c08a52bcee402
nelsontchanyap79 <nelsontchanyap75@gmail.com>
2023-08-25 03:43:09 +0100

    HTML Struture of Index ,  About Us page

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,21 @@
 <!-- ======= Header ======= -->
+<div class="header-annotation">
+    <div class="social-network">
+        <div>Follow Us</div>
+        <a href=""><span class="bi-instagram"></span></a>
+        <a href=""><span class="bi-facebook"></span></a>
+        <a href=""><span class="bi-twitter"></span></a>
+        <a href=""><span class="bi-youtube"></span></a>
+    </div>
+    <div class="phone-number">
+      <div class="bi-telephone">
+        <a href="tel:+237 654 21 78 14"><span>+237 654 21 78 14</span></a>
+      </div>
+      <div class="bi-mailbox">
+        <a href="mailto:info@mypiole.com"><span>info@mypiole.com</span></a>
+      </div>
+    </div>
+</div><!--  Header Top  -->
 <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
@@ -14,6 +31,7 @@
             <ul>
               <li><a href="about/">About MyPiole</a></li>
               <li><a href="about/team.php">About Our Team</a></li>
+              <li><a class="nav-link scrollto " href="portfolio/">Our Portfolio</a></li>  
             </ul>
              
           <li><a class="nav-link scrollto" href="services/">Services</a></li>
@@ -30,7 +48,6 @@
               <li><a href="#">Shops</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto " href="portfolio/">Portfolio</a></li>
           <li><a class="nav-link scrollto" href="member/">Membership</a></li>
           <li><a href="blog.html">Blog</a></li>
           <li class="dropdown"><a href="#"><span>Account</span> <i class="bi bi-chevron-down"></i></a>
@@ -40,6 +57,7 @@
             </ul>
              
           <li><a class="nav-link scrollto" href="contact/">Contact Us</a></li>
+          <li id="Search-Link"><a class="nav-link"> <span>Search</span><i class="bi-search" id="Search-Button" ></i></a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
